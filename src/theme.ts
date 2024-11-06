d2b45ad711c83a33cece5819285386bc5bd46563
rexcape <53886131+rexcape@users.noreply.github.com>
2024-11-06 16:59:11 +0800

    fix: update cursor type

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 import { MantineTheme } from '@mantine/core'
 
 const theme: Partial<MantineTheme> = {
+  cursorType: 'pointer',
   fontFamily: '"Inter", sans-serif',
   fontFamilyMonospace: '"SF Mono", monospace',
   primaryColor: 'indigo',
